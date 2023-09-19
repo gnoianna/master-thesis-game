c062148b88cc8c8cb5cd8cc0d228a76ffe020de8
Anna Gnoińska <ania.gnoinska@gmail.com>
2023-09-19 20:45:17 +0200

    Small changes

--- Assets/Scripts/UI/ButtonHoverHandler.cs
+++ Assets/Scripts/UI/ButtonHoverHandler.cs
@@ -16,9 +16,9 @@ public class ButtonHoverHandler : MonoBehaviour, IPointerEnterHandler, IPointerE
     public GameMode mode;
 
     private string originalText = "Choose your option!\n\n<b>Hover over the button and see mode description</b>";
-    private string easyModeText = "Easy mode here";
-    private string mediumModeText = "Medium mode here";
-    private string hardModeText = "Hard mode here";
+    private string easyModeText = "<b>Easy mode</b>\n\nYou can use both hands and legs to destroy obstacles. It doesn't matter which part of the body you will use!\n\nHave fun and try to store as many points as possible!";
+    private string mediumModeText = "<b>Medium mode</b>\n\nPink elements have to be destroyed using left part of the body, similarly blue ones can be collected only with right part of the body.\n\nBe careful and remember about the rules!";
+    private string hardModeText = "<b>Hard mode</b>\n\nYour hands can collect green spheres, but blue ones can be destroyed only using your legs!\n\nYou have to react quickly, obstacles are coming really fast!";
 
 
     private void Awake()
